e0711d9836ef235c1784d0a2f1105ee923032fa3
herbiesoft <herb@herbiesoft.com>
2019-10-10 20:27:51 -0500

    save data

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public static readonly LoggerFactory MyConsoleLoggerFactory = new LoggerFactory(new []
+        {
+            new ConsoleLoggerProvider((category, level)
+                => category == DbLoggerCategory.Database.Command.Name 
+                   && level == LogLevel.Information,true)
+
+        });
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .UseSqlServer(
                 "Server = (localdb)\\mssqllocaldb; Database=SamuraiAppData; Trusted_Connection = True; ");
         }
 
--- SomeUI/Program.cs
+++ SomeUI/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using SamuraiApp.Data;
 
 namespace SomeUI
 {
@@ -10,6 +14,36 @@ namespace SomeUI
     {
         static void Main(string[] args)
         {
+            InsertSamurai();
+
+
+            Console.ReadKey();
+        }
+
+        private static void InsertSamurai()
+        {
+            
+            var samurai = new Samurai {Name = "Herb"};
+            using (var context = new SamuraiContext())
+            {
+
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        context.Samurais.Add(samurai);
+                        context.SaveChanges();
+
+                        transaction.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        
+                    }
+                }
+
+            }
         }
     }
 }
